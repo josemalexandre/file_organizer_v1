c52b8917ccaa9386ad5c8ac20c8f08578b20f1e9
José Maria Alexandre Silva <josemalexandre@outlook.com>
2023-09-09 12:34:38 -0300

    Correção de arquivo
    Correção para que o script desconsiderasse as pastas.

--- agrupa.py
+++ agrupa.py
@@ -1,17 +1,18 @@
 import os
 
+camiho = os.getcwd()
+
 diretorios_arquivos = []
 
 for i in os.listdir():
-    diretorios_arquivos.append(f'{os.getcwd()}\{i}')
+    if os.path.isfile(i):
+        diretorios_arquivos.append(f'{camiho}\{i}')
 
 extensoes = set()
 
 for i in diretorios_arquivos:
     if os.path.splitext(i)[1].replace('.', '').upper() != 'PY':
         extensoes.add(os.path.splitext(i)[1].replace('.', '').upper())
-    else:
-        continue
 
 for i in extensoes:
     os.mkdir(f'{i}')
@@ -19,4 +20,4 @@ for i in extensoes:
 for i in diretorios_arquivos:
     for j in extensoes:
         if os.path.splitext(i)[1].replace('.', '').upper() == j:
-            os.rename(i, f'{os.getcwd()}\{j}\{os.path.split(i)[1]}')
+            os.rename(i, f'{camiho}\{j}\{os.path.split(i)[1]}')
